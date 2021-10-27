9c58b55b0a8ba7e8be3c87e66febc85ece565d1d
Jake Metherell <jake@fluffylogic.com>
2021-10-27 11:19:17 +0100

    Bump version of chips-app module to pull in nagios fix

--- groups/chips-ef-batch/main.tf
+++ groups/chips-ef-batch/main.tf
@@ -28,7 +28,7 @@ provider "vault" {
 }
 
 module "chips-ef-batch" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/chips-app?ref=1.0.86"
+  source = "git@github.com:companieshouse/terraform-modules//aws/chips-app?ref=1.0.87"
 
   application                      = var.application
   application_type                 = "chips"
