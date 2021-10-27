0a8c07a22a7144e44e336bae9eb057164e13350d
Jake Metherell <jake@fluffylogic.com>
2021-10-27 12:33:18 +0100

    Bump version of chips-app module

--- groups/chips-ef-batch/main.tf
+++ groups/chips-ef-batch/main.tf
@@ -28,7 +28,7 @@ provider "vault" {
 }
 
 module "chips-ef-batch" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/chips-app?ref=1.0.87"
+  source = "git@github.com:companieshouse/terraform-modules//aws/chips-app?ref=1.0.88"
 
   application                      = var.application
   application_type                 = "chips"
