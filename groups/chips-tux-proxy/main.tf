54f81a392ef8a97b98af17971bdc5b5732519a00
Jake Metherell <jake@fluffylogic.com>
2021-12-06 18:08:33 +0000

    Reference version 1.0.96 of chips-app module
    This pulls in the change to reduce IAM policy size

--- groups/chips-tux-proxy/main.tf
+++ groups/chips-tux-proxy/main.tf
@@ -28,7 +28,7 @@ provider "vault" {
 }
 
 module "chips-tux-proxy" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/chips-app?ref=1.0.95"
+  source = "git@github.com:companieshouse/terraform-modules//aws/chips-app?ref=1.0.96"
 
   application                      = var.application
   application_type                 = "chips"
