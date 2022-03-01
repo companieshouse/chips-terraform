3185feb9498a77c80e4d9197d6a1ea1173e9116b
Matt Stone <m.stone@kainos.com>
2022-03-01 11:36:35 +0000

    fix instance attribute name

--- groups/chips-db/ssm.tf
+++ groups/chips-db/ssm.tf
@@ -7,7 +7,7 @@ resource "aws_ssm_association" "ansible_check" {
 
   targets {
     key    = "InstanceIds"
-    values = aws_instance.db_ec2.*.instance_id
+    values = aws_instance.db_ec2.*.id
   }
 
   name = "ch-ssm-run-ansible"
@@ -48,7 +48,7 @@ resource "aws_ssm_association" "ansible_apply" {
 
   targets {
     key    = "InstanceIds"
-    values = aws_instance.db_ec2.*.instance_id
+    values = aws_instance.db_ec2.*.id
   }
 
   name = "ch-ssm-run-ansible"
