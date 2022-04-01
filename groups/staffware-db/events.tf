e8f670a7eec0ec29a02d09b6f8d22ff4cac31519
Matt Stone <m.stone@kainos.com>
2022-04-01 16:03:36 +0100

    Initial events setup for DB failover triggers

---
+++ groups/staffware-db/events.tf
@@ -0,0 +1,24 @@
+resource "aws_cloudwatch_event_rule" "failover_alarm_rule" {
+  name          = "${var.application}_db_failure"
+  description   = "Rule for triggering failover actions based on ${var.application} db alarms."
+  event_pattern = <<EOF
+{
+  "source": ["aws.cloudwatch"],
+  "detail-type": ["CloudWatch Alarm State Change"],
+  "detail": {
+    "alarmName": [
+      "${module.cloudwatch-alarms.ec2_systemstatus_check.alarm_name}",
+      "${module.cloudwatch-alarms.ec2_overallstatus_check.alarm_name}",
+      "${module.cloudwatch-alarms.ec2_instancestatus_check.alarm_name}"
+    ]
+  }
+}
+EOF
+}
+
+resource "aws_cloudwatch_event_target" "failover_event_target" {
+  target_id = "${var.application}DBSSMFailoverDocument"
+  arn       = aws_ssm_document.failover_db.arn
+  rule      = aws_cloudwatch_event_rule.failover_alarm_rule.name
+  role_arn  = aws_iam_role.ssm_runbook_execution_perms.arn
+}
