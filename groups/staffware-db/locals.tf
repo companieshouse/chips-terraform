1b30b8c32d88f25f55c499338e7ce043d041d0a6
Marty Fox <martin.fox@kainos.com>
2022-04-13 10:34:20 +0100

    Spelling on local value causing failed plan

--- groups/staffware-db/locals.tf
+++ groups/staffware-db/locals.tf
@@ -12,7 +12,7 @@ locals {
   security_s3_data        = data.vault_generic_secret.security_s3_buckets.data
   ec2_data                = data.vault_generic_secret.ec2_data.data
   kms_keys_data           = data.vault_generic_secret.kms_keys.data
-  security_kms_keys_data  = data.vault_generic_secret.security_kms_keys.da
+  security_kms_keys_data  = data.vault_generic_secret.security_kms_keys.data
   ssm_data                = data.vault_generic_secret.ssm.data
 
   logs_kms_key_id        = local.kms_keys_data["logs"]
