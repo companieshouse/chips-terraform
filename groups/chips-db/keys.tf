68cb4b7d8d0f5a5eefa133c56ffafc88bd0acf99
Marty Fox <martin.fox@kainos.com>
2022-04-13 15:01:58 +0100

    Updating chips-db application var to chips-oltp so that the EC2 are specific per DB

--- groups/chips-db/keys.tf
+++ groups/chips-db/keys.tf
@@ -3,6 +3,6 @@
 # ------------------------------------------------------------------------------
 
 resource "aws_key_pair" "ec2_keypair" {
-  key_name   = format("%s-db", var.application)
+  key_name   = "chips-db"
   public_key = local.ec2_data["public-key"]
 }
