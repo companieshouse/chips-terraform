d8ccf06a764734f0bc685c0dc13a34221d2be1a5
Matt Stone <m.stone@kainos.com>
2022-04-15 10:57:06 +0100

    Adding DNS failover to ssm automation

--- groups/chips-rep-db/ssm.tf
+++ groups/chips-rep-db/ssm.tf
@@ -86,6 +86,8 @@ resource "aws_ssm_document" "failover_db" {
       db_instance_name            = "${var.application}-db-*"
       command_document_name       = "ch-ssm-run-ansible"
       command_document_parameters = indent(8, yamlencode(merge(local.ansible_ssm_parameters, { Check = "False" })))
+      dns_name                    = aws_route53_record.dns_cname.name
+      route53_zone                = data.aws_route53_zone.private_zone.zone_id
     }
   )
   tags = merge(
