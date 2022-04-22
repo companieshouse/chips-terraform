2edaddc3f958471c07d62975ebc12f9c8fdb9d01
Matt Stone <m.stone@kainos.com>
2022-04-22 15:21:54 +0100

    missing permissions

--- groups/staffware-db/iam.tf
+++ groups/staffware-db/iam.tf
@@ -96,6 +96,7 @@ data "aws_iam_policy_document" "ssm_runbook_execution_perms" {
     actions = [
       "ec2:DescribeInstances",
       "ec2:StartInstances",
+      "ec2:StopInstances",
       "ec2:DescribeInstanceStatus"
     ]
     resources = ["*"]
