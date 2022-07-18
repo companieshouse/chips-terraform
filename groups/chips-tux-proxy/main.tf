de489ed9e36c3e4a9251a0667a3d759d92550f19
nevans4-ch <nevans4@companieshouse.gov.uk>
2022-07-18 14:34:06 +0100

    CM-1414 add permission for cloudwatch metrics
    Updating version of chips-app module to pick up changes

--- groups/chips-tux-proxy/main.tf
+++ groups/chips-tux-proxy/main.tf
@@ -28,7 +28,7 @@ provider "vault" {
 }
 
 module "chips-tux-proxy" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/chips-app?ref=1.0.168"
+  source = "git@github.com:companieshouse/terraform-modules//aws/chips-app?ref=1.0.169"
 
   application                      = var.application
   application_type                 = "chips"
