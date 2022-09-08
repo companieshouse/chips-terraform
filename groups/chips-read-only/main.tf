ec12d4f9039125b58f08b75e895e2980a38a0658
Jake Metherell <jake@fluffylogic.com>
2022-09-08 07:30:57 +0100

    Reference 1.0.175 of chips-app terraform module

--- groups/chips-read-only/main.tf
+++ groups/chips-read-only/main.tf
@@ -28,7 +28,7 @@ provider "vault" {
 }
 
 module "chips-read-only" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/chips-app?ref=1.0.169"
+  source = "git@github.com:companieshouse/terraform-modules//aws/chips-app?ref=1.0.175"
 
   application                      = var.application
   application_type                 = "chips"
