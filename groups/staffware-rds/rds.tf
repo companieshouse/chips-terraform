1bd29891bd040c6b041d251194eeff599913d2b5
Wayne Jenkins <wjenkins1@companieshouse.gov.uk>
2023-02-14 13:10:18 +0000

    Add config to deploy staffware-rds in heritage-development-eu-west-2

---
+++ groups/staffware-rds/rds.tf
@@ -0,0 +1,141 @@
+# ------------------------------------------------------------------------------
+# RDS Security Group and rules
+# ------------------------------------------------------------------------------
+module "rds_security_group" {
+
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 3.0"
+
+  name        = "sgr-${var.identifier}-rds-001"
+  description = "Security group for the ${var.identifier} RDS database"
+  vpc_id      = data.aws_vpc.vpc.id
+
+  ingress_rules            = ["oracle-db-tcp"]
+  ingress_prefix_list_ids  = [data.aws_ec2_managed_prefix_list.administration.id]
+
+  ingress_with_cidr_blocks = []
+  ingress_with_source_security_group_id = []
+
+  egress_rules = ["all-all"]
+}
+
+resource "aws_security_group_rule" "oem_rule" {
+  description       = "Oracle Enterprise Manager"
+  from_port         = 5500
+  to_port           = 5500
+  protocol          = "tcp"
+  type              = "ingress"
+  prefix_list_ids   = [data.aws_ec2_managed_prefix_list.administration.id]
+  security_group_id = module.rds_security_group.this_security_group_id
+}
+
+# ------------------------------------------------------------------------------
+# RDS Instance
+# ------------------------------------------------------------------------------
+module "staffware_rds" {
+  source  = "terraform-aws-modules/rds/aws"
+  version = "2.23.0"
+
+  create_db_parameter_group = true
+  create_db_subnet_group    = true
+
+  identifier                 = "rds-${var.identifier}-${var.environment}-001"
+  engine                     = "oracle-se2"
+  major_engine_version       = var.major_engine_version
+  engine_version             = var.engine_version
+  auto_minor_version_upgrade = var.auto_minor_version_upgrade
+  license_model              = var.license_model
+  instance_class             = var.instance_class
+  allocated_storage          = var.allocated_storage
+  storage_type               = var.storage_type
+  iops                       = var.iops
+  multi_az                   = var.multi_az
+  storage_encrypted          = true
+  kms_key_id                 = data.aws_kms_key.rds.arn
+
+  name     = upper(var.name)
+  username = local.staffware_rds_data["admin-username"]
+  password = local.staffware_rds_data["admin-password"]
+  port     = "1521"
+
+  deletion_protection       = true
+  maintenance_window        = var.rds_maintenance_window
+  backup_window             = var.rds_backup_window
+  backup_retention_period   = var.backup_retention_period
+  skip_final_snapshot       = false
+  final_snapshot_identifier = "${var.identifier}-final-deletion-snapshot"
+
+  # Enhanced Monitoring
+  monitoring_interval             = "30"
+  monitoring_role_arn             = data.aws_iam_role.rds_enhanced_monitoring.arn
+  enabled_cloudwatch_logs_exports = var.rds_log_exports
+
+  performance_insights_enabled          = var.performance_insights_enabled
+  performance_insights_kms_key_id       = data.aws_kms_key.rds.arn
+  performance_insights_retention_period = 7
+
+  # RDS Security Group
+  vpc_security_group_ids = [
+    module.rds_security_group.this_security_group_id,
+    data.aws_security_group.rds_shared.id
+  ]
+
+  # DB subnet group
+  subnet_ids = data.aws_subnet_ids.data.ids
+
+  # DB Parameter group
+  family = "oracle-se2-${var.major_engine_version}"
+
+  parameters = var.parameter_group_settings
+
+  options = [
+    {
+      option_name                    = "OEM"
+      port                           = "5500"
+      vpc_security_group_memberships = [module.rds_security_group.this_security_group_id]
+    },
+    {
+      option_name = "SQLT"
+      version     = "2018-07-25.v1"
+      option_settings = [
+        {
+          name  = "LICENSE_PACK"
+          value = "N"
+        },
+      ]
+    },
+    {
+      option_name = "Timezone"
+      option_settings = [
+        {
+          name  = "TIME_ZONE"
+          value = "Europe/London"
+        },
+      ]
+    }
+  ]
+
+  timeouts = {
+    "create" : "80m",
+    "delete" : "80m",
+    "update" : "80m"
+  }
+
+  tags = merge(
+    local.default_tags,
+    map(
+      "ServiceTeam", "${upper(var.identifier)}-DBA-Support"
+    )
+  )
+}
+
+module "rds_cloudwatch_alarms" {
+  source = "git@github.com:companieshouse/terraform-modules//aws/oracledb_cloudwatch_alarms?ref=tags/1.0.173"
+
+  db_instance_id         = module.staffware_rds.this_db_instance_id
+  db_instance_shortname  = upper(var.name)
+  alarm_actions_enabled  = var.alarm_actions_enabled
+  alarm_name_prefix      = "Oracle RDS"
+  alarm_topic_name       = var.alarm_topic_name
+  alarm_topic_name_ooh   = var.alarm_topic_name_ooh
+}
