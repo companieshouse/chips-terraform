87e83bd4eb4e923142fa1c0696999a0711d00002
Jake Metherell <jake@fluffylogic.com>
2023-05-18 09:17:55 +0100

    Remove accidental change to module path

--- groups/chips-users-rest/main.tf
+++ groups/chips-users-rest/main.tf
@@ -28,7 +28,7 @@ provider "vault" {
 }
 
 module "chips-users-rest" {
-  source = "./chips-app"
+  source = "git@github.com:companieshouse/terraform-modules//aws/chips-app?ref=1.0.183"
 
   application                        = var.application
   application_type                   = "chips"
