b3eb1ff48224dfe089785eb45d32cdf5112370cc
Marc Ransome <mransome@companieshouse.gov.uk>
2023-10-25 14:18:19 +0100

    Remove SNS topic subscription

--- groups/chips-reginit/sns.tf
+++ groups/chips-reginit/sns.tf
@@ -12,13 +12,3 @@ resource "aws_sns_topic_subscription" "chips-reginit_Subscription" {
     aws_sns_topic.chips-reginit_topic
   ]
 }
-
-resource "aws_sns_topic_subscription" "chips-reginit_Subscriptionhttps" {
-  topic_arn = aws_sns_topic.chips-reginit_topic.arn
-  protocol  = "https"
-  endpoint  = "https://companieshouse.xmatters.com/api/integration/1/functions/663f6327-04ae-46ab-8e8d-f5[…]Key=850133a0-4cae-477e-9b69-c0cbc8154d8c&recipients=unix"
-
-  depends_on = [
-    aws_sns_topic.chips-reginit_topic
-  ]
-}
