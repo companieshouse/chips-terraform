6920d935795813f4c6fc63db3fbe4a1cd61e6207
samh241 <98320840+samh241@users.noreply.github.com>
2023-10-31 15:19:52 +0000

    Update groups/chips-reginit/sns.tf
    Co-authored-by: Marc Ransome <mransome@companieshouse.gov.uk>

--- groups/chips-reginit/sns.tf
+++ groups/chips-reginit/sns.tf
@@ -13,7 +13,7 @@ resource "aws_sns_topic_subscription" "chips-reginit_Subscription" {
   ]
 }
 
-resource "aws_sns_topic_subscription" "chips-reginit_Subscriptionhttps" {
+resource "aws_sns_topic_subscription" "chips_reginit_subscription_https" {
   topic_arn = aws_sns_topic.chips-reginit_topic.arn
   protocol  = "https"
   endpoint  = data.vault_generic_secret.sns_url.data["url"]
