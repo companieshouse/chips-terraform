09f2a3df38fedc260922f562d8753f207a80c26e
Jake Metherell <jake@fluffylogic.com>
2024-02-21 13:05:48 +0000

    Update chips-app module ref to pull in CIS scanning policy

--- groups/chips-read-only/main.tf
+++ groups/chips-read-only/main.tf
@@ -28,7 +28,7 @@ provider "vault" {
 }
 
 module "chips-read-only" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/chips-app?ref=1.0.245"
+  source = "git@github.com:companieshouse/terraform-modules//aws/chips-app?ref=1.0.251"
 
   application                        = var.application
   application_type                   = "chips"
