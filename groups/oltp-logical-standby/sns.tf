82c167568c8d495fec8bc856589a074f2d1a3641
Chris Harris <charris1@companieshouse.gov.uk>
2024-03-01 11:37:57 +0000

    updated sns details

--- groups/oltp-logical-standby/sns.tf
+++ groups/oltp-logical-standby/sns.tf
@@ -3,7 +3,7 @@ resource "aws_sns_topic" "oltp-logstb_topic" {
 }
 
 resource "aws_sns_topic_subscription" "oltp-logstb_Subscription" {
-  topic_arn = aws_sns_topic.chips-dev_topic.arn
+  topic_arn = aws_sns_topic.oltp-logstb_topic.arn
   for_each  = toset(["linuxsupport@companieshouse.gov.uk"])
   protocol  = "email"
   endpoint  = each.value
@@ -14,7 +14,7 @@ resource "aws_sns_topic_subscription" "oltp-logstb_Subscription" {
 }
 
 resource "aws_sns_topic_subscription" "oltp-logstb_Subscriptionhttps" {
-  topic_arn = aws_sns_topic.chips-dev_topic.arn
+  topic_arn = aws_sns_topic.oltp-logstb_topic.arn
   protocol  = "https"
   endpoint  = data.vault_generic_secret.sns_url.data["url"]
 
