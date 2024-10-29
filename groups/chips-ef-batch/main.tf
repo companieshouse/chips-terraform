d454564809e903c83b3845e23c9672f62e9478c5
0x4348 <sbeechey@companieshouse.gov.uk>
2024-10-29 10:40:00 +0000

    k

--- groups/chips-ef-batch/main.tf
+++ groups/chips-ef-batch/main.tf
@@ -36,6 +36,8 @@ module "chips-ef-batch" {
   aws_account                        = var.aws_account
   account                            = var.account
   region                             = var.region
+  short_account                      = var.short_account
+  short_region                       = var.short_region
   environment                        = var.environment
   ami_name                           = var.ami_name
   asg_count                          = var.asg_count
